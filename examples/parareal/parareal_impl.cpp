65ddd659c320a56abe2b24ac089b6b5636be36b7
selmanTerzi <sterzi@hotmail.de>
2015-04-01 15:33:41 +0200

    fixed some semantic errors

--- examples/parareal/parareal_impl.cpp
+++ examples/parareal/parareal_impl.cpp
@@ -1,4 +1,7 @@
 
+#include <iostream>
+#include <vector>
+
 #include <pfasst.hpp>
 #include <pfasst/config.hpp>
 #include <pfasst/logging.hpp>
@@ -22,10 +25,10 @@ namespace pfasst
       class Parareal {
 
       private:
-        list<shared_ptr<SDC<time>>>  g; // coarse-level controllers
-        list<shared_ptr<SDC<time>>> f; // fine-level controllers
+        vector<shared_ptr<SDC<time>>>  g; // coarse-level controllers
+        vector<shared_ptr<SDC<time>>> f; // fine-level controllers
         
-        const ITransfer<time> transfer;
+        shared_ptr<SpectralTransfer1D<time>> transfer;
         
         shared_ptr<SDC<time>> getSDC(const size_t nnodes, const size_t ndofs, const double dt,
                                      const double startTime, const quadrature::QuadratureType quad_type) {
@@ -42,7 +45,7 @@ namespace pfasst
           sdc->add_level(sweeper);
 
           // 1 SDC controller for each time step
-          sdc->set_duration(startTime, startTime+dt, dt, 1);
+          sdc->set_duration(startTime, startTime+dt, dt, 2);
           sdc->setup();
           
           auto q0 = sweeper->get_start_state();
@@ -55,9 +58,9 @@ namespace pfasst
                   const size_t ndofs, const quadrature::QuadratureType quad_type) {
           g.clear();
           f.clear();
-          for(int i= 0 ; i< nsteps; i++) {
-            g.insert(getSDC((nnodes-1)/3, ndofs/2, dt, i*dt, quad_type));
-            f.insert(getSDC(nnodes, ndofs, dt, i*dt, quad_type));
+          for(size_t i = 0 ; i < nsteps; i++) {
+            g.push_back(getSDC((nnodes-1)/3, ndofs/2, dt, i*dt, quad_type));
+            f.push_back(getSDC(nnodes, ndofs, dt, i*dt, quad_type));
           }
           transfer = make_shared<SpectralTransfer1D<>>();
         }
@@ -77,41 +80,45 @@ namespace pfasst
           shared_ptr<Encapsulation<double>> fineState(nullptr);
           shared_ptr<Encapsulation<double>> deltaFineState(nullptr);
           
-          for(int i = 0; i < niters; i++) {
-            for(int j = 0; j < nsteps; j++) {
+          for(size_t i = 0; i < niters; i++) {
+            for(size_t j = 0; j < nsteps; j++) {
               if(i > j + 1) continue;
               
               coarse = *(g.begin()+j);
               fine = *(f.begin()+j);
               
-              EncapSweeper<time> coarseSweeper = as_encap_sweeper<time>(coarse->finest());
-              EncapSweeper<time> fineSweeper = as_encap_sweeper<time>(fine->finest());
+              EncapSweeper<time>& coarseSweeper = as_encap_sweeper<time>(coarse->get_finest());
+              EncapSweeper<time>& fineSweeper = as_encap_sweeper<time>(fine->get_finest());
               
               if(i > 0) {
-                transfer.interpolate(fineSweeper.get_start_state(), coarseSweeper.get_start_state());
-                fine.set_step(0);
-                fine.run();
+                transfer->interpolate(fineSweeper.get_start_state(), coarseSweeper.get_start_state());
+                fine->set_step(0);
+                fine->run();
                 deltaFineState->copy(fineSweeper.get_end_state());
-                transfer.interpolate(fineState, coarseSweeper.get_end_state());
+                fineState = fineSweeper.get_factory()->create(solution);
+                transfer->interpolate(fineState, coarseSweeper.get_end_state());
                 deltaFineState->saxpy(-1.0, fineState);
               }
               
               if(coarseState) {
                 coarseSweeper.get_start_state()->copy(coarseState);
               }
+              else {
+                coarseSweeper.get_factory()->create(solution);
+              }
               
-              coarse.set_step(0);
-              coarse.run();
+              coarse->set_step(0);
+              coarse->run();
               coarseState->copy(coarseSweeper.get_end_state());
               
               if(deltaFineState) {
-                transfer.interpolate(fineSweeper.get_end_state(), coarseState);
+                transfer->interpolate(fineSweeper.get_end_state(), coarseState);
                 fineSweeper.get_end_state()->saxpy(1.0, deltaFineState);
-                transfer.restrict(coarseSweeper.get_end_state(),fineSweeper.get_end_state());
+                transfer->restrict(coarseSweeper.get_end_state(),fineSweeper.get_end_state());
               }
             }
             if(i > 0) {
-              coarseState = as_encap_sweeper<time>((*(g.begin()+i-1))->finest()).get_end_state();
+              coarseState = as_encap_sweeper<time>((*(g.begin()+i-1))->get_finest()).get_end_state();
             }
             else {
               coarseState.reset();
@@ -136,7 +143,7 @@ int main(int argc, char** argv)
   auto const ndofs  = 128;
   auto const quad_type = pfasst::quadrature::QuadratureType::GaussLegendre;
         
-  auto parareal = make_shared<pfasst::examples::parareal::Parareal<pfasst::time_precision>>();
-  parareal->run_parareal(nsteps,dt,nnodes,niters,ndofs,quad_type);
-  }
+  pfasst::examples::parareal::Parareal<> parareal;
+  parareal.run_parareal(nsteps,dt,nnodes,niters,ndofs,quad_type);
+}
 #endif
