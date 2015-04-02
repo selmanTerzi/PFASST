dbf96a19fad16241b5eaacb3a28c74d1f8e3b534
selmanTerzi <sterzi@hotmail.de>
2015-04-02 15:30:01 +0200

    Debugging issue with start states

--- examples/parareal/parareal_impl.cpp
+++ examples/parareal/parareal_impl.cpp
@@ -29,11 +29,11 @@ namespace pfasst
         
         shared_ptr<SpectralTransfer1D<time>> transfer;
         
-        shared_ptr<SDC<time>> getSDC(const size_t nnodes, const size_t ndofs, const double dt,
-                                     const double startTime, const quadrature::QuadratureType quad_type) {
+        shared_ptr<SDC<time>> getSDC(const size_t nnodes, const size_t ndofs, 
+                                     const double dt, const double startTime) {
           auto sdc = make_shared<SDC<time>>();
           
-          auto quad = quadrature::quadrature_factory(nnodes, quad_type);
+          auto quad = quadrature::quadrature_factory(nnodes, pfasst::quadrature::QuadratureType::GaussLobatto);
 
           auto factory = make_shared<VectorFactory<double>>(ndofs);
           auto sweeper = make_shared<AdvectionDiffusionSweeper<>>(ndofs);
@@ -45,7 +45,7 @@ namespace pfasst
           sdc->add_level(sweeper);
 
           // 1 SDC controller for each time step
-          sdc->set_duration(startTime, startTime+dt, dt, 20);
+          sdc->set_duration(startTime, startTime+dt, dt, 10);
           sdc->setup();
           
           auto q0 = sweeper->get_start_state();
@@ -54,14 +54,14 @@ namespace pfasst
           return sdc;
         }
         
-        void init(const size_t nsteps, const double dt, const size_t nnodes, 
-                  const size_t ndofs, const quadrature::QuadratureType quad_type) {
+        void init(const size_t nsteps, const double dt, 
+                  const size_t nnodes, const size_t ndofs) {
           g.clear();
           f.clear();
           for(size_t i = 0 ; i < nsteps; i++) {
 //             g.push_back(getSDC((nnodes-1)/3, ndofs/2, dt, i*dt, quad_type));
-            f.push_back(getSDC(nnodes, ndofs, dt, i*dt, quad_type));
-            g.push_back(getSDC(nnodes, ndofs, dt, i*dt, quad_type));
+            f.push_back(getSDC(nnodes, ndofs, dt, i*dt));
+            g.push_back(getSDC(nnodes, ndofs, dt, i*dt));
           }
           transfer = make_shared<SpectralTransfer1D<>>();
         }
@@ -69,10 +69,9 @@ namespace pfasst
       public:
         
         void run_parareal(const size_t nsteps, const size_t niters,
-                          const double dt, const size_t nnodes, const size_t ndofs, 
-                          const quadrature::QuadratureType quad_type) {
+                          const double dt, const size_t nnodes, const size_t ndofs) {
           
-          init(nsteps, dt, nnodes, ndofs, quad_type);
+          init(nsteps, dt, nnodes, ndofs);
           
           shared_ptr<SDC<time>> coarse;
           shared_ptr<SDC<time>> fine;
@@ -80,7 +79,7 @@ namespace pfasst
           shared_ptr<Encapsulation<double>> coarseState(nullptr);
           shared_ptr<Encapsulation<double>> fineState(nullptr);
           shared_ptr<Encapsulation<double>> deltaFineState(nullptr);
-          shared_ptr<Encapsulation<double>> startState(nullptr);
+          shared_ptr<Encapsulation<double>> debugState(nullptr);
           
           for(size_t k = 0; k < niters; k++) {
             for(size_t n = 0; n < nsteps; n++) {
@@ -94,22 +93,29 @@ namespace pfasst
               EncapSweeper<time>& coarseSweeper = as_encap_sweeper<time>(coarse->get_finest());
               EncapSweeper<time>& fineSweeper = as_encap_sweeper<time>(fine->get_finest());
               
-              if(!startState) {
-                startState = coarseSweeper.get_factory()->create(solution);
-                startState->copy(coarseSweeper.get_start_state());
-              }
+              // DEBUG:
+              if(!debugState) debugState = coarseSweeper.get_factory()->create(solution);
+              debugState->copy(coarseSweeper.get_start_state());
+              debugState->saxpy(-1.0, coarseSweeper.get_state(0));
+              std::cout << "Debug-Error Coarse: " << debugState->norm0() << "\n";
+              debugState->copy(fineSweeper.get_start_state());
+              debugState->saxpy(-1.0, fineSweeper.get_state(0));
+              std::cout << "Debug-Error Fine: " << debugState->norm0() << "\n";
                 
               if(k > 0) {
                 if(n > 0) transfer->interpolate(fineSweeper.get_start_state(), coarseSweeper.get_start_state());
+                
                 std::cout << "Fine-Sweep" << "\n";
+                
                 fine->set_step(0);
                 fine->run();
-                // Calculate the correction value
+                
                 if(!deltaFineState) deltaFineState = fineSweeper.get_factory()->create(solution);
                 deltaFineState->copy(fineSweeper.get_end_state());
-                // Interpolated coarse value
+                
                 if(!fineState) fineState = fineSweeper.get_factory()->create(solution);
                 transfer->interpolate(fineState, coarseSweeper.get_end_state());
+                
                 deltaFineState->saxpy(-1.0, fineState);
               }
               
@@ -118,16 +124,12 @@ namespace pfasst
               }
               else {
                 coarseState = coarseSweeper.get_factory()->create(solution);
-                coarseState->copy(coarseSweeper.get_start_state());
-                coarseState->saxpy(-1.0, startState);
-                std::cout << "startState-Error: " << coarseState->norm0() << "\n";
-                
               }
               
               std::cout << "Coarse-Sweep" << "\n";
               coarse->set_step(0);
               coarse->run();
-              if(n < nsteps -1) coarseState->copy(coarseSweeper.get_end_state());
+              coarseState->copy(coarseSweeper.get_end_state());
               
               if(deltaFineState) {
                 transfer->interpolate(fineState, coarseState);
@@ -155,14 +157,13 @@ int main(int argc, char** argv)
   pfasst::init(argc, argv,
                pfasst::examples::parareal::AdvectionDiffusionSweeper<>::init_opts,
                pfasst::examples::parareal::AdvectionDiffusionSweeper<>::init_logs);
-  const size_t  nsteps = 5;
+  const size_t  nsteps = 4;
   const double  dt     = 0.01;
-  const size_t  niters = 3;
-  const size_t  nnodes = 8;
-  const size_t  ndofs  = 128;
-  const pfasst::quadrature::QuadratureType quad_type = pfasst::quadrature::QuadratureType::GaussLegendre;
+  const size_t  niters = 2;
+  const size_t  nnodes = 3;
+  const size_t  ndofs  = 64;
   
   pfasst::examples::parareal::Parareal<> parareal;
-  parareal.run_parareal(nsteps,niters,dt,nnodes,ndofs,quad_type);
+  parareal.run_parareal(nsteps,niters,dt,nnodes,ndofs);
 }
 #endif
