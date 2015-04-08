f093253d0e515b5e201e7b28c02c9d6ff4e67088
selmanTerzi <sterzi@hotmail.de>
2015-04-08 14:01:31 +0200

    examples: parareal: made some changes. program uses only 2 controller instances instead of a list. Solution is saved at fine level.

--- examples/parareal/parareal.cpp
+++ examples/parareal/parareal.cpp
@@ -23,38 +23,42 @@ namespace pfasst
     namespace parareal 
     {
       template<typename time = time_precision>
-      class Parareal {
+      class Parareal 
+      {
 
         private:
-          vector<shared_ptr<SDC<time>>>  g; // coarse-level controllers
-          vector<shared_ptr<SDC<time>>> f; // fine-level controllers
+          shared_ptr<SDC<time>> coarse; // coarse-level controller
+          shared_ptr<SDC<time>> fine; // fine-level controller
           
           shared_ptr<SpectralTransfer1D<time>> transfer;
           
-          shared_ptr<Encapsulation<double>> uStart;
+          shared_ptr<EncapFactory<>> factory; // fine level factory
           
-          vector<shared_ptr<Encapsulation<double>>> u; // vector with numerical solution
-          vector<shared_ptr<Encapsulation<double>>> uExact; // vector with exact solution
+          vector<shared_ptr<Encapsulation<double>>> u; // vector with current numerical solution at fine level
+          vector<shared_ptr<Encapsulation<double>>> uCoarse; // vector with interpolated coarse-Sweep results
+          vector<shared_ptr<Encapsulation<double>>> uFine; // vector with fine-Sweep results
+          vector<shared_ptr<Encapsulation<double>>> uExact; // vector with exact solution at fine level
           shared_ptr<Encapsulation<double>> err;
           error_map errors;
           
-          shared_ptr<SDC<time>> getSDC(const size_t nnodes, const size_t ndofs, 
-                                       const double dt, const double startTime, const bool fine) {
+          double dt;
+          size_t nfineiters;
+          size_t ncrseiters;
+          
+          shared_ptr<SDC<time>> getSDC(const size_t nnodes, const size_t ndofs)
+          {
             auto sdc = make_shared<SDC<time>>();
             
             auto quad = quadrature::quadrature_factory(nnodes, pfasst::quadrature::QuadratureType::GaussLobatto);
-
+            
             auto factory = make_shared<VectorFactory<double>>(ndofs);
             auto sweeper = make_shared<AdvectionDiffusionSweeper<>>(ndofs);
             
             sweeper->set_residual_tolerances(1e-8, 0.0);
             sweeper->set_quadrature(quad);
             sweeper->set_factory(factory);
-
+            
             sdc->add_level(sweeper);
-
-            // 1 SDC controller for each time step
-            sdc->set_duration(startTime, startTime+dt, dt, fine ? 10 : 1);
             sdc->setup();
             
             auto q0 = sweeper->get_start_state();
@@ -63,28 +67,68 @@ namespace pfasst
             return sdc;
           }
         
-          void init(const size_t nsteps, const double dt, 
-                    const size_t nnodes, const size_t ndofs) {
+          void init(const size_t nsteps, const double dt, const size_t nnodes, 
+                    const size_t ndofs_fine, const size_t ndofs_coarse,
+                    const size_t nfineiters, const size_t ncrseiters) 
+          {
             
-            size_t crsendofs = ndofs/2;
+            factory = make_shared<VectorFactory<double>>(ndofs_fine);
+            auto sweeper = make_shared<AdvectionDiffusionSweeper<>>(ndofs_fine);
+            this->dt = dt;
+            this->nfineiters = nfineiters;
+            this->ncrseiters = ncrseiters;
             
-            auto factory = make_shared<VectorFactory<double>>(ndofs);
-            auto sweeper = make_shared<AdvectionDiffusionSweeper<>>(ndofs);
+            fine = getSDC(nnodes, ndofs_fine);
+            coarse = getSDC(nnodes, ndofs_coarse);
+//             coarse = getSDC(nnodes/3, ndofs_coarse, dt, i*dt, false);
             
-            g.clear();
-            f.clear();
-            for(size_t i = 0 ; i < nsteps; i++) {
-//               g.push_back(getSDC(nnodes/3, crsendofs, dt, i*dt, false));
-              f.push_back(getSDC(nnodes, ndofs, dt, i*dt, true));
-              g.push_back(getSDC(nnodes, ndofs, dt, i*dt, true));
-              
-              u.push_back(factory->create(solution));
-              uExact.push_back(factory->create(solution));
-              sweeper->exact(uExact[i],i*dt);
-            }
             err = factory->create(solution);
-            uStart = factory->create(solution);
             transfer = make_shared<SpectralTransfer1D<>>();
+            
+            u.clear();
+            uFine.clear();
+            uCoarse.clear();
+            uExact.clear();
+            
+            for(size_t i = 0 ; i < nsteps; i++) {
+              u.push_back(factory->create(solution));
+              uFine.push_back(factory->create(solution));
+              uCoarse.push_back(factory->create(solution));
+              uExact.push_back(factory->create(solution));
+              sweeper->exact(uExact[i],(i+1)*dt);
+            }
+          }
+          
+          void do_coarse(shared_ptr<Encapsulation<>> start_state,
+                         shared_ptr<Encapsulation<>> end_state,
+                         const double startTime) 
+          {
+            coarse->set_duration(startTime, startTime+dt, dt, ncrseiters);
+            EncapSweeper<time>& coarseSweeper = as_encap_sweeper<time>(coarse->get_finest());
+            
+            if(start_state) transfer->restrict(coarseSweeper.get_start_state(), start_state);
+                
+            CLOG(INFO, "Parareal")  << "Coarse-Sweep" << endl;
+            coarse->run();
+            
+            CVLOG(1, "Parareal") << "End_state coarse" << vector<double>(as_vector<double,time>(coarseSweeper.get_end_state()));
+            transfer->interpolate(end_state, coarseSweeper.get_end_state());
+            CVLOG(1, "Parareal") << "End_state interpolated" << vector<double>(as_vector<double,time>(end_state));
+          }
+          
+          void do_fine(shared_ptr<Encapsulation<>> start_state,
+                       shared_ptr<Encapsulation<>> end_state,
+                       const double startTime) 
+          {
+            fine->set_duration(startTime, startTime+dt, dt, nfineiters);
+            EncapSweeper<time>& fineSweeper = as_encap_sweeper<time>(fine->get_finest());
+            
+            if(start_state) fineSweeper.get_start_state()->copy(start_state);
+                
+            CLOG(INFO, "Parareal")  << "Fine-Sweep" << endl;
+            fine->run();
+            
+            end_state->copy(fineSweeper.get_end_state());
           }
           
         public:
@@ -94,6 +138,9 @@ namespace pfasst
           {
             pfasst::examples::parareal::AdvectionDiffusionSweeper<>::init_opts();
             pfasst::config::options::add_option<size_t>("Parareal", "num_par_iter", "num Parareal iterations");
+            pfasst::config::options::add_option<size_t>("Parareal", "spatial_dofs_coarse", "num Parareal iterations");
+            pfasst::config::options::add_option<size_t>("Parareal", "num_fine_iter", "num Fine Sweep iterations");
+            pfasst::config::options::add_option<size_t>("Parareal", "num_crse_iter", "num Coarse Sweep iterations");
           }
           
           static void init_logs()
@@ -103,83 +150,51 @@ namespace pfasst
           }
         
           void run_parareal(const size_t nsteps, const size_t niters,
-                            const double dt, const size_t nnodes, const size_t ndofs)
+                            const size_t nfineiters, const size_t ncrseiters,
+                            const double dt, const size_t nnodes, 
+                            const size_t ndofs_fine, const size_t ndofs_coarse)
           {
             
-            init(nsteps, dt, nnodes, ndofs);
+            init(nsteps, dt, nnodes, ndofs_fine, ndofs_coarse, nfineiters, ncrseiters);
             
-            shared_ptr<SDC<time>> coarse;
-            shared_ptr<SDC<time>> fine;
-            
-            shared_ptr<Encapsulation<double>> coarseState(nullptr);
-            shared_ptr<Encapsulation<double>> deltaCourseState(nullptr);
+            shared_ptr<Encapsulation<double>> coarseState = factory->create(solution);
+            shared_ptr<Encapsulation<double>> fineState = factory->create(solution);
+            shared_ptr<Encapsulation<double>> delta = factory->create(solution);
             
             for(size_t k = 0; k < niters; k++) {
               for(size_t n = 0; n < nsteps; n++) {
                 if(k > n + 1) continue;
                 
-                coarse = g[n];
-                fine = f[n];
+                CLOG(INFO, "Parareal") << "k: " << k << " n: " << n << endl;
                 
-                std::cout << "k: " << k << " n: " << n << "\n";
+                if(n >= k) do_coarse(n > 0 ? u[n-1] : nullptr, coarseState, n*dt);
+                if(n >= k) do_fine(n > 0 ? u[n-1] : nullptr, fineState, n*dt);
                 
-                EncapSweeper<time>& coarseSweeper = as_encap_sweeper<time>(coarse->get_finest());
-                EncapSweeper<time>& fineSweeper = as_encap_sweeper<time>(fine->get_finest());
-                  
                 if(k > 0) {
-                  // calculate fine sweep and delta correction
-                  CVLOG(1, "Parareal") << fineSweeper.get_start_state() << ": " << vector<double>(as_vector<double, time>(fineSweeper.get_start_state()));
-                  CVLOG(1, "Parareal") << coarseSweeper.get_start_state() << ": " << vector<double>(as_vector<double, time>(coarseSweeper.get_start_state()));
-                  if(n > 0) transfer->interpolate(fineSweeper.get_start_state(), coarseSweeper.get_start_state());
-                  CVLOG(1, "Parareal") << fineSweeper.get_start_state() << ": " << vector<double>(as_vector<double, time>(fineSweeper.get_start_state()));
+                  u[n]->copy(uFine[n]);
                   
-                  u[n]->copy(coarseSweeper.get_start_state());
-                  err->copy(uExact[n]);
-                  err->saxpy(-1.0,u[n]);
-                  std::cout << "Error: " << err->norm0() << "\n";
-                  errors.insert(vtype(ktype(n, k-1), err->norm0()));
-                  
-                  std::cout << "Fine-Sweep" << std::endl;
-                  fine->set_step(0);
-                  fine->run();
-                  
-                  CLOG(INFO, "Parareal") << "Calculating delta";
-                  CVLOG(4, "Parareal") << "verbose";
-                  
-                  if(!deltaCourseState) deltaCourseState = coarseSweeper.get_factory()->create(solution);
-                  transfer->restrict(deltaCourseState, fineSweeper.get_end_state());
-                  deltaCourseState->saxpy(-1.0, coarseSweeper.get_end_state());
-                  
-                  std::cout << "deltaCourseState Norm: " << deltaCourseState->norm0() << "\n";
-                }
-                
-                if(coarseState) {
-                  coarseSweeper.get_start_state()->copy(coarseState);
-                  std::cout << "Coarse-StartState Copy, Norm: " << coarseState->norm0() << "\n";
+                  if(n >= k) {
+                    CLOG(INFO, "Parareal") << "Calculating delta";
+                    
+                    delta->copy(coarseState);
+                    delta->saxpy(-1.0, uCoarse[n]);
+                    
+                    CLOG(INFO, "Parareal") << "delta Norm: " << delta->norm0() << endl;
+                    
+                    // apply delta correction
+                    u[n]->saxpy(1.0, delta);
+                  }
                 }
                 else {
-                  coarseState = coarseSweeper.get_factory()->create(solution);
+                  u[n]->copy(coarseState);
                 }
+                uCoarse[n]->copy(coarseState);
+                uFine[n]->copy(fineState);
                 
-                std::cout << "Coarse-StartState Norm: " << coarseSweeper.get_start_state()->norm0() << "\n";
-                
-                std::cout << "Coarse-Sweep" << "\n"; 
-                coarse->set_step(0);
-                coarse->run();
-                
-                coarseState->copy(coarseSweeper.get_end_state());
-                
-                if(k > 0) {
-                  // apply delta correction
-                  coarseState->saxpy(1.0, deltaCourseState);
-                  if(n == k-1) uStart->copy(coarseState);
-                }
-              } // loop over time steps
-              if(k > 0) {
-                coarseState->copy(uStart);
-              }
-              else {
-                coarseState.reset();
+                err->copy(uExact[n]);
+                err->saxpy(-1.0, u[n]);
+                CLOG(INFO, "Parareal") << "Error: " << err->norm0() << endl;
+                errors.insert(vtype(ktype(n, k-1), err->norm0()));
               }
             } // loop over parareal iterations
           } // function run_parareal
@@ -194,13 +209,16 @@ int main(int argc, char** argv)
   pfasst::init(argc, argv,
                pfasst::examples::parareal::Parareal<>::init_opts,
                pfasst::examples::parareal::Parareal<>::init_logs);
-  const size_t  nsteps = 8;
+  const size_t  nsteps = 5;
   const double  dt     = 0.01;
-  const size_t  niters = 5;
-  const size_t  nnodes = 9;
-  const size_t  ndofs  = 64;
+  const size_t  npariters = pfasst::config::get_value<size_t>("num_par_iter", 5);
+  const size_t  nfineiters = pfasst::config::get_value<size_t>("num_fine_iter", 10);
+  const size_t  ncrseiters = pfasst::config::get_value<size_t>("num_crse_iter", 5);
+  const size_t  nnodes = pfasst::config::get_value<size_t>("num_nodes", 9);
+  const size_t  ndofs_fine  = pfasst::config::get_value<size_t>("spatial_dofs", 64);
+  const size_t  ndofs_coarse  = pfasst::config::get_value<size_t>("spatial_dofs_coarse", 64);
   
   pfasst::examples::parareal::Parareal<> parareal;
-  parareal.run_parareal(nsteps,niters,dt,nnodes,ndofs);
+  parareal.run_parareal(nsteps, npariters, nfineiters, ncrseiters, dt, nnodes, ndofs_fine, ndofs_coarse);
 }
 #endif
